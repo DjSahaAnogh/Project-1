49e54ccb46ead1824ddb8a1529802f5715c72807
Dhanonjoy Saha Anogh <dhanonjoy6a24@gmail.com>
2023-12-16 12:52:01 +0600

    added

---
+++ JS/email.js
@@ -0,0 +1,31 @@
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: false, // adjust based on your server
+  auth: {
+    user: 'dhanonjoy6a24@gmail.com',
+    pass: 'govindha888',
+  },
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const name = document.getElementById(name).value;
+  const email = document.getElementById(email).value;
+  const message = document.getElementById(message).value;
+
+  const mailOptions = {
+    from: name + ' <' + email + '>',
+    to: 'dhanonjoy6a24@gmail.com',
+    subject: 'New enquiry',
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      alert('Error sending email:', error);
+    } else {
+      alert('Email sent successfully!');
+    }
+  });
+}
